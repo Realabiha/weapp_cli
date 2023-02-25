8f5a0560962e64546f5affb51ac6804cbdc3c3de
Realabiha <m17386550687@163.com>
2023-02-25 18:35:03 +0800

    update: 测试axios-wx-request

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 // app.js
 // import { sum } from '@/utils/util'
-// import axios from 'axios-wx-request'
-import axios from '@/utils/axios-wx-request'
+import axios from 'axios-wx-request'
 const { cancelPromise, cancelFunc } = axios.cancelFunc()
 console.log(cancelPromise)
 axios({ url: 'https://www.baidu.com', cancelPromise }).then(res => console.log(res), rej => console.log(rej))
