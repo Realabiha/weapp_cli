f12ed8e59734f8a4154705bf66efe4fad8444324
Realabiha <m17386550687@163.com>
2023-06-12 11:50:33 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 // app.js
 // import { sum } from '@/utils/util'
-import axios from 'axios-wx-request'
+// import axios from 'axios-wx-request'
+import axios from '@/utils/axios-wx-request'
 const { cancelPromise, cancelFunc } = axios.cancelFunc()
 console.log(cancelPromise)
+console.log(axios, 'axios')
+axios.intercepter.request.use(_ => { console.log(11); return _ }, _ => _)
+axios.intercepter.request.use(_ => { console.log(22); return _ }, _ => _)
+axios.intercepter.response.use(_ => { console.log(33); return _ }, _ => _)
+axios.intercepter.response.use(_ => { console.log(44); return _.data }, _ => _)
 axios({ url: 'https://www.baidu.com', cancelPromise }).then(res => console.log(res), rej => console.log(rej))
-cancelFunc()
+// cancelFunc()
 App({
   async onLaunch() {
     console.log('123')
